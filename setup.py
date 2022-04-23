e8178eb43dc8760b647f46bd12c88a1916d46ff8
keviny2 <kevinyang10@gmail.com>
2022-04-23 14:42:16 -0700

    change src.cli:main to src.cli:run in setup.py

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ setup(
     packages=find_packages(),
     entry_points={
         'console_scripts': [
-            'liquid-bayes=src.cli:main',
+            'liquid-bayes=src.cli:run',
         ]
     }
 )
